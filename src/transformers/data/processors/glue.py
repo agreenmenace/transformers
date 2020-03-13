8c13794f15f934d6254d03bf259457500a11a65e
Christine Shen <42813818+agreenmenace@users.noreply.github.com>
2020-03-13 11:42:06 -0400

    Update glue.py

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -534,7 +534,7 @@ class BoolQProcessor(DataProcessor):
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(self._read_tsv(os.path.join(data_dir, "val.jsonl")), "val")
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "val.jsonl")), "dev")
 
     def get_labels(self):
         """See base class."""
